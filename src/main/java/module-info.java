beeb96258609ed82b69159326f92e1fb37d6b61f
Julio González <juliogtrenard@gmail.com>
2025-01-07 09:23:21 +0100

    UPDATE exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,14 @@ module es.juliogtrenard.listadopaises {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires net.sf.jasperreports.core;
 
 
     opens es.juliogtrenard.listadopaises to javafx.fxml;
     exports es.juliogtrenard.listadopaises;
+    exports es.juliogtrenard.listadopaises.controller;
+    opens es.juliogtrenard.listadopaises.controller to javafx.fxml;
+    exports es.juliogtrenard.listadopaises.db;
+    exports es.juliogtrenard.listadopaises.dao;
+    exports es.juliogtrenard.listadopaises.model;
 }
